02d39757c68afc7d7f76bd49ce983900c5014014
DongEun02 <ehddms0517@naver.com>
2025-09-10 23:31:56 +0900

    feat: 테스트페이지 슬라이더 구현

--- src/pages/F1Test.jsx
+++ src/pages/F1Test.jsx
@@ -1,5 +1,44 @@
+import WeightSlider from '../components/WeightSlider';
+import LiveResultCard from '../components/LiveResultCard';
+import Button from '../components/Button';
+import './F1Test.css';
+import { useNavigate } from 'react-router-dom';
+
 const F1Test = () => {
-  return <div>F1Test</div>;
+  const f1SliderLabels = [
+    '팀 성적',
+    '선수 경력(성적)',
+    '근본',
+    '프랜차이즈 or 유스',
+    '언더독',
+    '팬덤 규모',
+  ];
+
+  const nav = useNavigate();
+  const goResultPage = () => {
+    nav('/result');
+  };
+
+  return (
+    <div className="f1test-page">
+      <div className="f1test-title">F1Test</div>
+      <div className="test-container">
+        <div className="slider-section">
+          <WeightSlider labels={f1SliderLabels} />
+        </div>
+        <div className="rank-section">
+          <LiveResultCard type={'f1'} />
+        </div>
+      </div>
+      <div className="button-section">
+        <Button
+          text="결과 보러 가기"
+          type="resultPage"
+          onClick={goResultPage}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default F1Test;
