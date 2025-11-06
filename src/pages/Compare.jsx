5754ae0f7be926aefde17beb08aeed0e86d7a48f
김동은 <ehddms0517@naver.com>
2025-11-06 13:35:44 +0900

    styles: styled-components로 변경

--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -1,8 +1,44 @@
 import Chart from '../components/compare/Chart';
 import Button from '../components/common/Button';
-import styles from './Compare.module.css';
 import { useState } from 'react';
 import { f1Teams, kboTeams } from '../utils/teamData';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+  height: 100vh;
+`;
+
+const Header = styled.div`
+  text-align: center;
+  margin-top: 20px;
+`;
+
+const TeamButton = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: center;
+`;
+
+const TeamSelect = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+
+const TeamOption = styled.select`
+  background-color: #374151;
+  color: white;
+  border: none;
+  padding: 8px;
+  border-radius: 5px;
+  width: 170px;
+  margin-right: 30px;
+`;
 
 const Compare = () => {
   const [type, setType] = useState('f1');
@@ -39,22 +75,22 @@ const Compare = () => {
   };
 
   return (
-    <div className={styles['compare-page']}>
-      <div className={styles['header']}>
+    <Container>
+      <Header>
         <h2>팀 전력 비교</h2>
         <p>두 팀의 전력을 한눈에 비교해 보세요.</p>
-      </div>
-      <div className={styles['button-section']}>
+      </Header>
+      <TeamButton>
         <Button onClick={changeTypeF1} text="F1팀 비교" type="compare-f1" />
         <Button
           onClick={changeTypeKBO}
           text="KBO팀 비교"
           type="compare-baseball"
         />
-      </div>
-      <div className={styles['team-select-section']}>
-        팀1 :{' '}
-        <select onChange={onSelectTeam1}>
+      </TeamButton>
+      <TeamSelect>
+        팀1 :
+        <TeamOption onChange={onSelectTeam1}>
           <option value="">선택</option>
           {type === 'f1'
             ? f1Teams.map((team) => (
@@ -67,9 +103,9 @@ const Compare = () => {
                   {team.name}
                 </option>
               ))}
-        </select>
-        팀2 :{' '}
-        <select onChange={onSelectTeam2}>
+        </TeamOption>
+        팀2 :
+        <TeamOption onChange={onSelectTeam2}>
           <option value="">선택</option>
           {type === 'f1'
             ? f1Teams.map((team) => (
@@ -82,16 +118,16 @@ const Compare = () => {
                   {team.name}
                 </option>
               ))}
-        </select>
-      </div>
-      <div className={styles['chart-section']}>
+        </TeamOption>
+      </TeamSelect>
+      <div>
         {team1 && team2 ? (
           <Chart team1={team1} team2={team2} />
         ) : (
           <p>비교할 팀을 선택해 주세요.</p>
         )}
       </div>
-    </div>
+    </Container>
   );
 };
 
