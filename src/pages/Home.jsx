7c0bc0139980fabd4abd2fc8048ef7adcebbb22d
김동은 <ehddms0517@naver.com>
2025-11-09 23:11:44 +0900

    styles: 홈페이지 styled-components로 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,132 +1,116 @@
-import styles from './Home.module.css';
 import { useNavigate } from 'react-router-dom';
 import Button from '../components/common/Button';
 import DescriptionBox from '../components/home/DescriptionBox';
 import HoverTeamName from '../components/home/HoverTeamName';
 import { DB_Description, TeamNames } from '../utils/homeData';
-
-import { Trophy, Zap, Users, Target, Heart, Star } from 'lucide-react';
+import { Trophy, Users, Target, Heart, Star } from 'lucide-react';
+import * as S from './Home.styles';
 
 const Home = () => {
   const nav = useNavigate();
-  const goF1Test = () => {
-    nav('/test/f1');
-  };
-  const goBaseballTest = () => {
-    nav('/test/baseball');
-  };
+  const goF1Test = () => nav('/test/f1');
+  const goBaseballTest = () => nav('/test/baseball');
 
   return (
-    <div className={styles['home_container']}>
-      {/* Main */}
-      <div className={styles['main']}>
-        <div className={styles['content-grid']}>
-          <div className={styles['left-content']}>
-            <div className={styles['first-text']}>
-              <h1 className={styles['main-title']}>
-                당신에게 딱 맞는
-                <span style={{ color: '#fbbf24' }}> 스포츠 팀</span>을
-                찾아보세요!
-              </h1>
-              <div className={styles['description']}>
-                F1과 KBO의 다양한 팀들 중에서 당신의 성향과 취향에 가장 잘 맞는
-                팀을 추천해드립니다.
-              </div>
-              <div className={styles['description']}>
-                간단한 테스트로 평생 응원할 팀을 만나보세요.
-              </div>
+    <S.HomeContainer>
+      <S.ContentGrid>
+        {/* 왼쪽 영역 */}
+        <S.LeftContent>
+          <S.FirstText>
+            <S.MainTitle>
+              당신에게 딱 맞는
+              <span style={{ color: '#fbbf24' }}> 스포츠 팀</span>을 찾아보세요!
+            </S.MainTitle>
+            <S.Description>
+              F1과 KBO의 다양한 팀들 중에서 당신의 성향과 취향에 가장 잘 맞는
+              팀을 추천해드립니다.
+            </S.Description>
+            <S.Description>
+              간단한 테스트로 평생 응원할 팀을 만나보세요.
+            </S.Description>
+          </S.FirstText>
+
+          <S.ButtonContainer>
+            <Button onClick={goF1Test} text="F1팀" type="f1-button" />
+            <Button onClick={goBaseballTest} text="KBO팀" type="kbo-button" />
+          </S.ButtonContainer>
+        </S.LeftContent>
+
+        {/* 오른쪽 영역 */}
+        <S.RightContent>
+          <S.IllustrationBox>
+            <S.CircleBox>
+              <Users style={{ width: '3rem', height: '3rem', color: '#fff' }} />
+            </S.CircleBox>
+            <div style={{ color: '#ffffff', fontWeight: 500 }}>
+              나만의 팀을 찾아보세요!
             </div>
-
-            {/* F1, KBO Buttons */}
-
-            <div className={styles['button-container']}>
-              <Button onClick={goF1Test} text={'F1팀'} type={'f1-button'} />
-              {/* <Zap className={styles['button-icon']} /> */}
-
-              <Button
-                onClick={goBaseballTest}
-                text={'KBO팀'}
-                type={'kbo-button'}
+            {TeamNames.map((b) => (
+              <HoverTeamName
+                key={b.name}
+                name={b.name}
+                color={b.color}
+                xy={b.xy}
+                delayClass={b.delayClass}
               />
-            </div>
-          </div>
+            ))}
+          </S.IllustrationBox>
+        </S.RightContent>
+      </S.ContentGrid>
 
-          <div className={styles['right-content']}>
-            <div className={styles['illustration-box']}>
-              <div className={styles['circle-box']}>
-                <Users className={styles['circle-icon']} />
-              </div>
-              <div
-                className={styles['circle-text']}
-                style={{ color: '#ffffff', fontWeight: '500' }}
-              >
-                나만의 팀을 찾아보세요!
-              </div>
-              {TeamNames.map((b) => (
-                <HoverTeamName
-                  key={b.name}
-                  name={b.name}
-                  color={b.color}
-                  xy={b.xy}
-                  delayClass={b.delayClass}
-                />
-              ))}
-            </div>
-          </div>
-        </div>
+      {/* 왜 이 테스트를 해야 하나요 */}
+      <section>
+        <S.DBHeader>
+          <S.DBTitle>왜 이 테스트를 해야 할까요?</S.DBTitle>
+          <S.DBDescription>
+            당신의 성향을 분석해서 가장 잘 맞는 팀을 찾아드립니다
+          </S.DBDescription>
+        </S.DBHeader>
 
-        {/* Why ~ */}
-        <section className={styles['descriptionBox_container']}>
-          <div className={styles['DB_header']}>
-            <h2 className={styles['DB_title']}>왜 이 테스트를 해야 할까요?</h2>
-            <p className={styles['DB_description']}>
-              당신의 성향을 분석해서 가장 잘 맞는 팀을 찾아드립니다
-            </p>
-          </div>
-          <div className={styles['DB_grid']}>
-            <DescriptionBox
-              des={DB_Description[0]}
-              icon={
-                <Star className={styles['icon']} style={{ color: 'C4B5FD' }} />
-              }
-            />
-            <DescriptionBox
-              des={DB_Description[1]}
-              icon={
-                <Target
-                  className={styles['icon']}
-                  style={{ color: 'FBBF24' }}
-                />
-              }
-            />
-            <DescriptionBox
-              des={DB_Description[2]}
-              icon={
-                <Users className={styles['icon']} style={{ color: 'FFFFFF' }} />
-              }
-            />
-            <DescriptionBox
-              des={DB_Description[3]}
-              icon={
-                <Heart className={styles['icon']} style={{ color: 'F87171' }} />
-              }
-            />
-            <DescriptionBox
-              des={DB_Description[4]}
-              icon={
-                <Trophy
-                  className={styles['icon']}
-                  style={{ color: 'FBBF24' }}
-                />
-              }
-            />
-          </div>
-        </section>
-      </div>
-
-      {/* Footer */}
-      <div className={styles['footer']}></div>
-    </div>
+        <S.DBGrid>
+          <DescriptionBox
+            des={DB_Description[0]}
+            icon={
+              <Star
+                style={{ width: '3rem', height: '3rem', color: '#C4B5FD' }}
+              />
+            }
+          />
+          <DescriptionBox
+            des={DB_Description[1]}
+            icon={
+              <Target
+                style={{ width: '3rem', height: '3rem', color: '#FBBF24' }}
+              />
+            }
+          />
+          <DescriptionBox
+            des={DB_Description[2]}
+            icon={
+              <Users
+                style={{ width: '3rem', height: '3rem', color: '#FFFFFF' }}
+              />
+            }
+          />
+          <DescriptionBox
+            des={DB_Description[3]}
+            icon={
+              <Heart
+                style={{ width: '3rem', height: '3rem', color: '#F87171' }}
+              />
+            }
+          />
+          <DescriptionBox
+            des={DB_Description[4]}
+            icon={
+              <Trophy
+                style={{ width: '3rem', height: '3rem', color: '#FBBF24' }}
+              />
+            }
+          />
+        </S.DBGrid>
+      </section>
+    </S.HomeContainer>
   );
 };
 
